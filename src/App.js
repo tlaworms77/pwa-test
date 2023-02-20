e3a88e36f3edcd4cffca38a14bdf5ac23f638a81
[ERP]심재근 <tlaworms77@douzone.com>
2023-02-20 16:58:34 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
+        <button
+          title='test'
+          value={'123123'}
+          onClick={() => {
+            // This variable will save the event for later use.
+            // window.deferredPrompt.prompt();
+          }}
+        >
+          test
+        </button>
       </header>
     </div>
   );
